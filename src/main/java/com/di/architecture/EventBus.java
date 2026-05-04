8940a0651fc132f93b72fd03cc5d5c1b8b7fdc6b
lingenj <jacob.van.lingen@moderne.io>
2026-05-04 14:37:33 +0200

    Add event publisher - Make it async

--- src/main/java/com/di/architecture/EventBus.java
+++ src/main/java/com/di/architecture/EventBus.java
@@ -4,17 +4,20 @@ import com.di.annotations.Service;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class EventBus {
     private final List<EventListenerInvoker> invokers = new ArrayList<>();
+    private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
 
     public void register(Object bean, Method method) {
         invokers.add(new EventListenerInvoker(bean, method));
     }
 
     public void publish(Object event) {
-        invokers.forEach(it -> it.invoke(event));
+        invokers.forEach(it -> executorService.submit(() -> it.invoke(event)));
     }
 
     private record EventListenerInvoker(Object bean, Method method) {
