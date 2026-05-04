8cdcb96546b0ff3ee5b6b5af716f5a9ae96b132a
lingenj <jacob.van.lingen@moderne.io>
2026-05-04 13:35:22 +0200

    Update to Java 25

--- src/main/java/com/di/Startup.java
+++ src/main/java/com/di/Startup.java
@@ -1,48 +1,40 @@
-package com.di;
-
 import com.di.annotations.*;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
+private static List<Class<?>> getSuitableClasses() {
+    return new Reflections("com.di", new SubTypesScanner(false))
+            .getSubTypesOf(Object.class).stream()
+            .filter(it -> it.isAnnotationPresent(Bean.class) ||
+                          it.isAnnotationPresent(Configuration.class) ||
+                          it.isAnnotationPresent(Repository.class) ||
+                          it.isAnnotationPresent(RestController.class) ||
+                          it.isAnnotationPresent(Service.class))
+            .toList();
+}
 
-public class Startup {
+private static void initializeClasses() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    final var suitableClasses = new ArrayList<>(getSuitableClasses());
+    final var initializedClasses = new HashMap<Class<?>, Object>();
 
-    private static List<Class<?>> getSuitableClasses() {
-        return new Reflections("com.di", new SubTypesScanner(false))
-                .getSubTypesOf(Object.class).stream()
-                .filter(it -> it.isAnnotationPresent(Bean.class) ||
-                         it.isAnnotationPresent(Configuration.class) ||
-                         it.isAnnotationPresent(Repository.class) ||
-                         it.isAnnotationPresent(RestController.class) ||
-                         it.isAnnotationPresent(Service.class))
-                .toList();
-    }
+    while (initializedClasses.size() != suitableClasses.size()) {
+        for (final var clazz : suitableClasses.stream().filter(it -> !initializedClasses.containsKey(it)).toList()) {
+            final var constructor = clazz.getDeclaredConstructors()[0]; // this demo uses the [0] shortcut because each class has at least and at most 1 public constructor
+            final var dependencies = Arrays.stream(constructor.getParameters())
+                    .map(it -> initializedClasses.get(it.getType()))
+                    .filter(Objects::nonNull)
+                    .toList();
 
-    private static void initializeClasses() throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        final var suitableClasses = new ArrayList<>(getSuitableClasses());
-        final var initializedClasses = new HashMap<Class<?>, Object>();
-
-        while (initializedClasses.size() != suitableClasses.size()) {
-            for (final var clazz : suitableClasses.stream().filter(it -> !initializedClasses.containsKey(it)).toList()) {
-                final var constructor = clazz.getDeclaredConstructors()[0]; // this demo uses the [0] shortcut because each class has at least and at most 1 public constructor
-                final var dependencies = Arrays.stream(constructor.getParameters())
-                        .map(it -> initializedClasses.get(it.getType()))
-                        .filter(Objects::nonNull)
-                        .toList();
-
-                if (constructor.getParameterCount() == dependencies.size()) {
-                    System.out.printf("Bean `%s` loaded. External dependencies: %s%n", clazz.getCanonicalName(), dependencies.stream().map(it -> it.getClass().getCanonicalName()).toList());
-                    initializedClasses.put(clazz, constructor.newInstance(dependencies.toArray()));
-                }
+            if (constructor.getParameterCount() == dependencies.size()) {
+                System.out.printf("Bean `%s` loaded. External dependencies: %s%n", clazz.getCanonicalName(), dependencies.stream().map(it -> it.getClass().getCanonicalName()).toList());
+                initializedClasses.put(clazz, constructor.newInstance(dependencies.toArray()));
             }
         }
-
-        System.out.println("All classes loaded");
     }
 
-    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        initializeClasses();
-    }
+    IO.println("All classes loaded");
+}
+
+void main() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    initializeClasses();
 }
